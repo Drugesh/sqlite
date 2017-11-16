f152de2cc2f3a1c8c9cb27c693fe78a6ad54b6cd
Drugesh <djaiswal211@gmail.com>
2017-11-16 20:03:11 +0530

    updated files

--- sqlitereading.py
+++ sqlitereading.py
@@ -4,17 +4,29 @@ import sys
 conn = sqlite3.connect('testDB.db')
 print ("Opened database successfully");
 
-cursor = conn.execute("SELECT id, name, address, salary from company")
+cursor = conn.execute("SELECT * from inventory")
 flag=0;
 for row in cursor:
     arg=sys.argv[1]
-    if arg in row[1]:
+    if arg in row:
         print( "ID = ", row[0])
-        print ("NAME = ", row[1])
-        print ("ADDRESS = ", row[2])
-        print ("SALARY = ", row[3], "\n")
+        print ("MACHINENAME = ", row[1])
+        print ("IP = ", row[2])
+        print ("RAM = ", row[3], "\n")
+	print ("DIMMSTATS = ", row[4])
+	print ("HDDBAY = ", row[5])
+	print ("SSD = ", row[6])
+	print ("SSDTYPE = ", row[7])
+	print ("CONSOLEIP = ", row[8])
+	print ("CONSOLE = ", row[9])
+	print ("NOOFHDD = ", row[10])
+	print ("HDDCAPACITY = ", row[11])
+	print ("STORAGE = ", row[12])
+	print ("OWNER = ", row[13])
+	print ("PROJECT = ", row[14])
+	print ("LEASETIME = ", row[15])
+	print ("TIMETOEXPIRE = ", row[16])
         flag=1;
-        print(row)
 
 if flag==0:
          print("Invalid Name")
--- untitled3.py
+++ untitled3.py
@@ -6,7 +6,7 @@ conn = sqlite3.connect('testDB.db')
 print ("Opened database successfully");
 
 conn.execute("INSERT INTO Inventory (ID,MACHINENAME,IP,RAM,DIMMSTATS,HDDBAY,SSD,SSDTYPE,CONSOLEIP,CONSOLE,NOOFHDD,HDDCAPACITY,STORAGE,OWNER,PROJECT,LEASETIME,TIMETOEXPIRE) \
-      VALUES (1, 'DJTest', '10.10.10.92', '16GB', 'NA','10.10.10.92','50GB','Flash','10.10.10.75','10.10.10.74','4', '500GB','1TB','Drugesh','ITR','2 months','4 days')");
+      VALUES (2, 'DJtest2', '10.10.10.94', '32GB', 'NA','10.10.10.92','50GB','Flash','10.10.10.78','10.10.10.74','15', '500GB','1TB','Drugesh','ITR','1 months','4 days')");
 
 
 conn.commit()
